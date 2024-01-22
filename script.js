3357def0603f2dc33736f2bf0b675781e12ea696
Chris Meysner <110308659+RobotTooth@users.noreply.github.com>
2024-01-22 22:36:49 +0000

    about section and profile pic

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuLinks = document.querySelector('.menu-links');
+    const menuLinksList = document.querySelectorAll('.menu-links a');
     const hamburgerIcon = document.querySelector('.hamburger-icon');
     const openCvBtn = document.getElementById("openCvBtn");
     const linkedInBtn = document.getElementById("LinkedIn");
     const gitHubBtn = document.getElementById("GitHub");
+    // const openContacts = document.getElementById("openContacts");
 
     hamburgerIcon.addEventListener("click", function() {
         menuLinks.classList.toggle("responsive");
         hamburgerIcon.classList.toggle("responsive");
     });
 
-    const menuLinksList = document.querySelectorAll('.menu-links a');
     menuLinksList.forEach(function(link) {
         link.addEventListener('click', function() {
             menuLinks.classList.remove('responsive');
@@ -19,9 +20,14 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     openCvBtn.addEventListener("click", function() {
-        const cvFilePath = './assets/Chris_Meysner_-_Front-end_Web_Developer.pdf';
+        const cvFilePath = './assets/Chris_Meysner-Frontend_Web_Developer.pdf';
         window.open(cvFilePath, '_blank');
     });
+
+    // openContacts.addEventListener("click", function() {
+    //     const contacts = '#contacts';
+    //     window.open(contacts, "_blank");
+    // });
 
     linkedInBtn.addEventListener("click", function() {
         const linkedInLink = 'https://www.linkedin.com/in/chris-meysner-3856ba166/';
